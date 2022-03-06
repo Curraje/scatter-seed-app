7691b2e98077b1e9535cdd673cdb2f5446817ecd
Curraje <rajecsingh0@gmail.com>
2022-03-06 17:38:24 -0500

    add react-native-paper dependency

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+import React from "react";
 import App from "./src/App";
 import { registerRootComponent } from "expo";
+import { Provider as PaperProvider } from "react-native-paper";
+
+function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
 
 // Main Entry Point for App
-registerRootComponent(App);
+registerRootComponent(Main);
