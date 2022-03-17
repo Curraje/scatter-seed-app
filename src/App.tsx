cb6e43fdfc18efeafbc059e28b883e36f64af7c3
Curraje <rajecsingh0@gmail.com>
2022-03-17 14:00:31 -0400

    add apollo client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Router from "./navigation";
 import { Provider as PaperProvider } from "react-native-paper";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import scatterSeedTheme from "./theme";
+import { isDevelopment } from "./utils/helper.utils";
+import { IP, PORT, PRODUCTION_URL } from "react-native-dotenv";
+
+const uri = isDevelopment
+  ? `http://${IP || "localhost"}:${PORT || 4000}/graphql`
+  : `${PRODUCTION_URL}/graphql`;
+
+console.log("IP", IP);
+console.log("PORT", PORT);
+console.log("URI", uri);
+
+const client = new ApolloClient({
+  uri: uri,
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <PaperProvider theme={scatterSeedTheme}>
-      <Router />
-    </PaperProvider>
+    <ApolloProvider client={client}>
+      <PaperProvider theme={scatterSeedTheme}>
+        <Router />
+      </PaperProvider>
+    </ApolloProvider>
   );
 }
--- src/screens/Calendar/Calendar.tsx
+++ src/screens/Calendar/Calendar.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, FlatList } from "react-native";
 import GlobalStyles from "../../theme/GlobalStyles";
 import styles from "./calendar.styles";
+import { gql, useQuery } from "@apollo/client";
+
+// FIXME: Temporary proof of concept
+const PLANTS_QUERY = gql`
+  query {
+    plants {
+      id
+      CommonName
+    }
+  }
+`;
 
 type CalendarPageProps = Navigation.AppTabsPageProps<"Calendar">;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const PlantItem = ({ plant }: any) => {
+  return (
+    <>
+      <Text>{plant.CommonName}</Text>
+    </>
+  );
+};
+
 export default function CalendarPage({ navigation }: CalendarPageProps) {
+  const { data, loading, error } = useQuery(PLANTS_QUERY);
+
+  useEffect(() => {
+    console.log("LOADING", loading);
+    console.log("ERROR", error);
+    if (!loading) console.log("PLANT DATA", data);
+  }, [data, loading, error]);
+
   const onPressHandler = () => {
     navigation.navigate("Home");
   };
@@ -21,6 +49,12 @@ export default function CalendarPage({ navigation }: CalendarPageProps) {
       >
         <Text style={styles.text}>Home</Text>
       </Pressable>
+      {/* FIXME: Temporary Proof of Concept */}
+      <FlatList
+        data={data?.plants}
+        renderItem={({ item }) => <PlantItem plant={item} />}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
