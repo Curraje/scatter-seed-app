c271ea331ee8caa6b4d5ba55ec4e68e724277a32
Curraje <rajecsingh0@gmail.com>
2022-03-19 03:27:59 -0400

    move isDevelopment to api and remove empty file

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,5 +1,6 @@
-import { isDevelopment } from "./helper.utils";
 import { IP, PORT, PRODUCTION_URL } from "react-native-dotenv";
+
+export const isDevelopment = process.env.NODE_ENV === "development";
 
 export const API_URL = isDevelopment
   ? `http://${IP || "localhost"}:${PORT || 4000}/graphql`
